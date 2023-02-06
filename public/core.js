2fc83ab552e24da378617e273f7037528bef67cc
hex <waverew@gmail.com>
2023-02-06 13:38:10 -0500

    added sliding animations

--- public/core.js
+++ public/core.js
@@ -10,6 +10,21 @@ async function getData(){
 getData();
 const textArray = ['a', 'b', 's', 'd', 'x', 'z', 'c', 'v', 'n', 'm', 'q', 'w', 'y', '/', 'u', 'h', 'j', 'l', 'e', 'f', '/', '//', " "];
 window.onload = () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+            else{
+                entry.target.classList.remove('show');
+            }
+        })
+    })
+    const hidElemns = document.querySelectorAll('.main-cont');
+    hidElemns.forEach((el) => {
+        observer.observe(el);
+    });
     const button = document.getElementById('test');
     const anim = document.getElementById('anim');
     const ran = 21000 // Math.floor(Math.random()*9000);
