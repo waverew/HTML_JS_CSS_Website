cba2f38d06a09743f13ed8bc2b9f6e1f9e102559
waverew <waverew@gmail.com>
2023-02-12 21:57:46 -0500

    started working on content

--- public/core.js
+++ public/core.js
@@ -10,24 +10,24 @@ async function getData(){
 getData();
 const textArray = ['a', 'b', 's', 'd', 'x', 'z', 'c', 'v', 'n', 'm', 'q', 'w', 'y', '/', 'u', 'h', 'j', 'l', 'e', 'f', '/', '//', " "];
 window.onload = () => {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry);
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
-            else{
+    //const observer = new IntersectionObserver((entries) => {
+      //  entries.forEach((entry) => {
+        //    console.log(entry);
+          //  if (entry.isIntersecting) {
+            //    entry.target.classList.add('show');
+            //}
+            /*else{
                 entry.target.classList.remove('show');
-            }
-        })
-    })
-    const hidElemns = document.querySelectorAll('.main-cont');
-    hidElemns.forEach((el) => {
-        observer.observe(el);
-    });
+            }*/
+       // })
+    //})
+    //const hidElemns = document.querySelectorAll('.main-cont');
+    //hidElemns.forEach((el) => {
+      //  observer.observe(el);
+    //});
     const button = document.getElementById('test');
     const anim = document.getElementById('anim');
-    const ran = 21000 // Math.floor(Math.random()*9000);
+    const ran = 7000 // Math.floor(Math.random()*9000);
     console.log(ran);
     setInterval(() => {
         let text = "";
